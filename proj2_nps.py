4bdf0c71e702c7c68c86a9525b2c621a75fecbbe
mhiro214 <52865253+mhiro214@users.noreply.github.com>
2021-03-20 16:38:31 -0400

    Completed part1&2

--- proj2_nps.py
+++ proj2_nps.py
@@ -1,6 +1,6 @@
 #################################
-##### Name:
-##### Uniqname:
+##### Name: Hiroyuki Makino #####
+##### Uniqname: mhiro       #####
 #################################
 
 from bs4 import BeautifulSoup
@@ -30,7 +30,22 @@ class NationalSite:
     phone: string
         the phone of a national site (e.g. '(616) 319-7906', '307-344-7381')
     '''
-    pass
+    def __init__(self, site_url):
+        html = requests.get(site_url)
+        html_text = html.text
+        soup = BeautifulSoup(html_text, 'html.parser')
+        self.name = soup.find_all('a', class_="Hero-title")[0].get_text().strip()
+        self.category = soup.find_all('span', class_="Hero-designation")[0].get_text().strip()
+        locality = soup.find_all('span', itemprop="addressLocality")[0].get_text().strip()
+        region = soup.find_all('span', itemprop="addressRegion")[0].get_text().strip()
+        self.address = locality + ", " + region
+        self.zipcode = soup.find_all('span', itemprop="postalCode")[0].get_text().strip()
+        self.phone = soup.find_all('span', itemprop="telephone")[0].get_text().strip()
+
+
+    def info(self):
+        return f'{self.name} ({self.category}): {self.address} {self.zipcode}'
+
 
 
 def build_state_url_dict():
@@ -46,7 +61,19 @@ def build_state_url_dict():
         key is a state name and value is the url
         e.g. {'michigan':'https://www.nps.gov/state/mi/index.htm', ...}
     '''
-    pass
+    dict_states = dict()
+    html = requests.get('https://www.nps.gov/index.htm')
+    html_text = html.text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    menu_states = soup.find_all('ul', class_="dropdown-menu SearchBar-keywordSearch")[0]
+    list_links = menu_states.find_all('a')
+    for link in list_links:
+        url = link.get('href')
+        url = 'https://www.nps.gov' + url
+        name = link.get_text().lower()
+        dict_states[name] = url
+
+    return dict_states
        
 
 def get_site_instance(site_url):
@@ -62,7 +89,8 @@ def get_site_instance(site_url):
     instance
         a national site instance
     '''
-    pass
+    ns = NationalSite(site_url)
+    return ns
 
 
 def get_sites_for_state(state_url):
@@ -98,4 +126,5 @@ def get_nearby_places(site_object):
     
 
 if __name__ == "__main__":
-    pass
+    ns = get_site_instance('https://www.nps.gov/yell/index.htm')
+    print(ns.info())
